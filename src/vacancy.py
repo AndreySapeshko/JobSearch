d09b9e581907afb13349973be470421803cb2ffa
Andrey Sapeshko <andrdd17@gmail.com>
2025-08-03 06:29:44 +0300

    Полям класса Vacancy и методам их валидации изменен режим доступа на приватный

--- src/vacancy.py
+++ src/vacancy.py
@@ -6,22 +6,22 @@ class Vacancy(BaseVacancy):
     зарплата (среднее значение), диапазон зарплаты, работодатель, описание, требования и ссылка.
     В классе реализованы магические методы сравнения по полю зарплата (среднее значение). """
 
-    salary_range: str
-    description: str
-    requirement: str
-    url: str
+    __salary_range: str
+    __description: str
+    __requirement: str
+    __url: str
 
     __slots__ = ('name', 'salary', 'salary_range', 'employer', 'requirement', 'description', 'url')
 
     def __init__(self, name: str, salary: int, salary_range: str, employer: str,
                  requirement: str, description: str, url: str) -> None:
-        self.name = name
-        self.salary = salary
-        self.salary_range = salary_range
-        self.employer = employer
-        self.description = description
-        self.requirement = requirement
-        self.url = url
+        self.__name = name
+        self.__salary = salary
+        self.__salary_range = salary_range
+        self.__employer = employer
+        self.__description = description
+        self.__requirement = requirement
+        self.__url = url
 
     def __str__(self) -> str:
         return f'Vacancy({self.name}, {self.salary}, {self.employer})'
