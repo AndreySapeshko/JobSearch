85186c419a1e9b66bbcce68f32922fdcc70e5413
Andrey Sapeshko <andrdd17@gmail.com>
2025-08-28 13:58:28 +0300

    В модуль main добвлено использование последнего созданного функционала

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from src.hh_api_request_handler import HhApiRequestHandler
 from src.hh_reader_vacancies import HhReaderVacancies
 from src.vacancies_handler import VacanciesHandler
 from src.json_file_handler import JsonFileHandler
+from src.db_manager import DBManager
 from config import PATH_HH_VACANCIES_JSON
 from tests.test_vacancy_handler import handler
 
@@ -27,12 +28,29 @@ async def main():
     hh_reader = HhReaderVacancies(number_of_pages)
     vacancies = hh_reader.get_vacancies()
     handler = VacanciesHandler(vacancies, num_top_vacancies, key_words)
-    handler.filter_vacancies()
+    vacancies = handler.filter_vacancies()
     top_vacancies = handler.get_top_vacancies()
     file_handler = JsonFileHandler()
     file_handler.write_in_file(top_vacancies)
+    db_manager = DBManager()
+    db_manager.update_database(vacancies)
     for vacancy in top_vacancies:
         print(vacancy)
+    print('\nВсе работодатели с количеством вакансий: ')
+    employers = db_manager.get_companies_and_vacancies_count()
+    for emploer in employers:
+        print(emploer)
+    print(f'\nСредняя зарплата по всем вакансиям: {db_manager.get_avg_salary()}')
+    print('\nВакансии с зарплатой выше средней: ')
+    high_vacancies = db_manager.get_vacancies_with_higher_salary()
+    for vacancy in high_vacancies:
+        print(vacancy)
+    key_word = 'middle'
+    print(f'\nВакансии название которых содержет {key_word}: ')
+    filtered_vacancies = db_manager.get_vacancies_with_keyword(key_word)
+    for vacancy in filtered_vacancies:
+        print(vacancy)
+
 
 
 if __name__ == '__main__':
