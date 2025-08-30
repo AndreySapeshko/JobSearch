403ac8be7719667ca27400999d5db05165ad0174
Andrey Sapeshko <andrdd17@gmail.com>
2025-08-30 09:39:09 +0300

    Внесены изменения в модуль main

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ import asyncio
 import json
 
 from src.hh_api_request_handler import HhApiRequestHandler
-from src.hh_reader_vacancies import HhReaderVacancies
-from src.vacancies_handler import VacanciesHandler
-from src.json_file_handler import JsonFileHandler
 from src.db_manager import DBManager
-from config import PATH_HH_VACANCIES_JSON
-from tests.test_vacancy_handler import handler
 
 
 async def main():
